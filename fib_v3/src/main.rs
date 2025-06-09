7b8477b3783ded5ea024336332de960db2b26065
rdcrwndcrn <ejung1@outlook.de>
2025-06-09 14:48:17 +0200

    SOLVED fib_v3

--- fib_v3/src/main.rs
+++ fib_v3/src/main.rs
@@ -7,19 +7,33 @@ struct Fibonacci {
     b: BigUint,
 }
 
+impl Fibonacci {
+    fn new() -> Self {
+        Fibonacci {
+            a:BigUint::from(0u32),
+            b:BigUint::from(1u32),
+        }
+    }
+}
+
 impl Iterator for Fibonacci {
     // @TODO Add correct type for alias "Item" and implement the method "next()"
     type Item = BigUint;
 
+    fn next(&mut self) -> Option<Self::Item>{
+        let current = self.a.clone();
+        let c = &self.a + &self.b;
+        self.a = self.b.clone();
+        self.b = c.clone();
+        Some(current)
+    }
+
 }
 
 fn main() {
-    let fib = Fibonacci {
-        a: BigUint::from(0_u32),
-        b: BigUint::from(1_u32),
-    };
+    let mut fib = Fibonacci::new();
 
-    println!("Enter number to calculate Fibonacci for:");
+    println!("Enter number to calculate the n-th Fibonacci for n =");
     let mut buffer = String::new();
     let _ = io::stdin().read_line(&mut buffer);
     let number = buffer
@@ -29,4 +43,19 @@ fn main() {
 
     // For the loop, we need to make use of a special method offered by Iterator trait
     // @TODO Implement the loop below
+    let mut iteration = BigUint::from(0u8);
+    let one = BigUint::from(1u8);
+    while &iteration < &number {
+        let x = fib.next().unwrap();
+        println!("{x}");
+        iteration += &one;
+    }
+    /*
+    let number = buffer.trim().parse::<usize>().unwrap_or(10);
+
+    // For the loop, we need to make use of a special method offered by Iterator trait
+    for value in fib.take(number) {
+        println!("{value} ");
+    }
+     */
 }
